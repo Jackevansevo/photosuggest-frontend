9783fedae7feefe1f6a772ac1fa4daeef571aadf
Jack Evans <jack@evans.gb.net>
2018-05-02 09:34:42 +0100

    Remove about view

--- src/Routing.elm
+++ src/Routing.elm
@@ -8,9 +8,7 @@ import UrlParser exposing (..)
 matchers : Parser (Route -> a) a
 matchers =
     oneOf
-        [ map HomeRoute (s "" <?> stringParam "q" <?> stringParam "license")
-        , map AboutRoute (s "about")
-        ]
+        [ map HomeRoute (s "" <?> stringParam "q" <?> stringParam "license") ]
 
 
 parseLocation : Location -> Route
